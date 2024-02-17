d3fd168246bbbebb7c2030b0a58671f5f1b5e19a
joelmiller1643@gmail.com <joelmiller1643@gmail.com>
2024-02-17 02:10:22 -0600

    faq page

--- localdata/index.ts
+++ localdata/index.ts
@@ -72,3 +72,55 @@ export const shippingAndReturns = [
     "Complimentary gift packaging",
 "Ships within 24 hours!"
 ]
+
+export const faqs = [
+    {
+      title: "Who is JOEL?",
+      content:
+        "JOEL is a design system for building performant, accessible and beautiful web experiences.",
+    },
+    {
+      title: "Lorem Yoyo mzfk?",
+      content:
+        "The Open Source Discount is available for everyone who is building an open source project. You can apply to the discount by sending an email to trump@acme.com",
+    },
+    {
+      title: "What is it?",
+      content:
+        "Yes, you can use Acme for your freelance projects. You can purchase the Freelancer License from our website.",
+    },
+    {
+      title: "What is your refund policy?",
+      content: "We do not provide refunds. However, we can help you with any issues you may have.",
+    },
+    {
+      title: "Can I cancel my subscription?",
+      content: "Yes, you can cancel and renew your subscription at any time.",
+    },
+    {
+      title: "How do I switch from quarterly to yearly subscription?",
+      content:
+        "You can switch from quarterly to yearly subscription by canceling your quarterly subscription and purchasing a yearly subscription.",
+    },
+    {
+      title: "Do you have monthly payment plans?",
+      content:
+        "No, we do not provide monthly payment plans. You can purchase a quarterly or yearly subscription.",
+    },
+    {
+      title: "Do you have discounts for my babies?",
+      content:
+        "Yes, we provide a 50% discount for students. You can apply to the discount by sending an email to support@acme.com",
+    },
+    {
+      title: "Do you have discounts for startups?",
+      content:
+        "Yes, we provide a 50% discount for startups. You can apply to the discount by sending an email to support@acme.com",
+    },
+    {
+      title: "How often do you release updates?",
+      content: "We release updates every two weeks.",
+    },
+  ];
+  
+  
